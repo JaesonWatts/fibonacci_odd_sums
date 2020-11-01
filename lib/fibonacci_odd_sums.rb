7b4bdefdf083aca8bd05a087cd1f97ee631d6bd6
jaesonwatts <jaeson.watts@gmail.com>
2020-11-01 15:45:18 -0700

    array generation working

--- lib/fibonacci_odd_sums.rb
+++ lib/fibonacci_odd_sums.rb
@@ -1,6 +1,29 @@
-require "fibonacci_odd_sums/version"
+require "./lib/fibonacci_odd_sums/version"
 
 module FibonacciOddSums
+
+  class FibonacciSequence
+
+    def initialize  
+      system('clear')
+      @sequence_array = [0,1]
+      self.generate
+      puts @sequence_array
+    end
+
+    def generate
+      # binding.pry
+      while @sequence_array[0-1] < 10000 do
+        sum_num = @sequence_array[0-2] + @sequence_array[0-1]
+        @sequence_array << sum_num 
+        
+      end
+    end
+
+
+  end
+  
+
   class Error < StandardError; end
   # Your code goes here...
 end
