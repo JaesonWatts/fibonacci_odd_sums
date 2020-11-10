76bbb85cb25817140a4b0b435f5c87ab4cc890ca
jaesonwatts <jaeson.watts@gmail.com>
2020-11-10 16:25:23 -0700

    began js solution

---
+++ javascript/fibonacciOddSums.js
@@ -0,0 +1,17 @@
+var fibonacciOddSums = {
+  fibonacciArray = [0,1],
+  oddNumbersArray = [b],
+  createFibonacciArray: function() {
+    while (this.fibonacciArray[0-1] < 10000) {
+      sum_num = this.fibonacciArray[0-2] + this.fibonacciArray[0-1]
+      this.fibonacciArray.push(sum_num)
+    }
+
+  },
+  extractOddNumbers: function() {
+
+  },
+  sumOddNumbers: function() {
+    
+  }
+}
